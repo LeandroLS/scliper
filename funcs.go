00cd1bed5850a7b865cc6b63d28f42e8794dfb71
Leandro Lima <llimas@outlook.com>
2021-11-02 14:09:33 -0300

    change flag messages and move funcs of main.go to funcs.go

---
+++ funcs.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+
+	"golang.org/x/net/html"
+)
+
+func HandleErr(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+func Map(vs []string, f func(string) string) []string {
+	vsm := make([]string, len(vs))
+	for i, v := range vs {
+		vsm[i] = f(v)
+	}
+	return vsm
+}
+func MakeRequest(site string) *http.Response {
+	resp, err := http.Get(site)
+	HandleErr(err)
+	return resp
+}
+func ParseHtml(r io.Reader) *html.Node {
+	doc, err := html.Parse(r)
+	HandleErr(err)
+	return doc
+}
+
+func CreateFile(name string, suffix string) *os.File {
+	name = CleanString(name)
+	file, err := os.Create(fmt.Sprintf("%s%s", name, suffix))
+	HandleErr(err)
+	return file
+}
+
+func WriteInFile(file *os.File, content []byte) {
+	_, err := file.Write(content)
+	HandleErr(err)
+}
+
+func GetHtmlTags(n *html.Node, rawHtmlTag string, htmlTagKey string, tags []string) []string {
+	if n.Type == html.ElementNode && n.Data == rawHtmlTag {
+		for _, a := range n.Attr {
+			if a.Key == htmlTagKey {
+				tags = append(tags, a.Val)
+			}
+		}
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		tags = GetHtmlTags(c, rawHtmlTag, htmlTagKey, tags)
+	}
+	return tags
+}
+
+func CleanString(name string) string {
+	name = strings.ReplaceAll(name, "https://", "")
+	name = strings.ReplaceAll(name, "http://", "")
+	name = strings.ReplaceAll(name, "/", "-")
+	return name
+}
+
+type File struct {
+	Name string
+	Size int64
+}
+
+func LogCreatedFileMessage(file File, fileType string) {
+	templateStr := `-------------------
+{{ type }} file created! 😀
+Name: {{ .Name }}
+Size: {{ .Size }} bytes
+-------------------
+`
+	tmpl, err := template.New("test").Funcs(template.FuncMap{"type": func() string {
+		if fileType == "HTML" {
+			return "HTML .html"
+		} else if fileType == "Links" {
+			return "Links .json"
+		} else {
+			return "Images .html"
+		}
+	}}).Parse(templateStr)
+	HandleErr(err)
+	err = tmpl.Execute(os.Stdout, file)
+	HandleErr(err)
+}
